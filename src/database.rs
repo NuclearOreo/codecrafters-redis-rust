99846a402465379401af3fe4c67151a77caebb77
Ussama Azam <nuclearoreo@gmail.com>
2024-02-07 23:12:44 -0500

    clean up

--- src/database.rs
+++ src/database.rs
@@ -67,41 +67,15 @@ impl DataBase {
         db.keys().cloned().collect()
     }
 
+    // Everything I need to know https://rdb.fnordig.de/file_format.html
     pub fn load(&self) -> Result<()> {
         if self.dbfilename.is_empty() {
             return Ok(());
         }
 
-        let mut index = 9;
-        let bytes = fs::read(format!("{}/{}", self.dir, self.dbfilename))?;
-
-        let s = String::from_utf8(bytes[..index].to_vec())?;
-        index += 1;
-        println!("{:?}", s);
-
-        let fb_pos = bytes.iter().position(|&b| b == 0xfb).unwrap() + 1;
-        let s = String::from_utf8(bytes[fb_pos + 8..fb_pos + 21].to_vec())?;
-        println!("{:?}", s);
-
-        // let code = (bytes[index] as u16) << 8 | (bytes[index + 1] as u16);
-        // println!("{:}", code);
+        // let mut index = 9;
+        // let bytes = fs::read(format!("{}/{}", self.dir, self.dbfilename))?;
 
         Ok(())
     }
 }
-
-// Ok(file) => {
-//     let mut buffer: [u8; 1024] = [0; 1024];
-//     let mut reader = std::io::BufReader::new(file);
-//     reader.read(&mut buffer).unwrap();
-//     println!("{:x?}", buffer);
-//     let fb_pos = buffer.iter().position(|&b| b == 0xfb).unwrap();
-//     let mut pos = fb_pos + 4;
-//     let len = buffer[pos];
-//     pos += 1;
-//     let key = &buffer[pos..(pos + len as usize)];
-//     println!("1 {:x?}", key);
-//     let pars = std::str::from_utf8(key).unwrap();
-//     println!("2 {:?}", pars);
-//     return Request::KEYS(vec![pars.to_string()]);
-// }
